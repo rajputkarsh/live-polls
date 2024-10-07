13cf8adec98f0252e604e12b15e792ef563a1be0
utkarsh <r.utkarsh.0010@gmail.com>
2024-10-07 10:46:24 +0530

    fix kinde auth mechanism

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -44,3 +44,41 @@ export async function GET() {
   // redirect the user to dashboard page
   return NextResponse.redirect(`${baseUrl}${routes.dashboard}`);
 }
+export async function POST() {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    return NextResponse.redirect(`${baseUrl}${routes.login}`);
+  }
+
+  // check if the user exists in the db
+  let dbUser = await prisma.user.findUnique({
+    where: { kindeId: user.id },
+  });
+
+  // user were not found, we will create it
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        kindeId: user.id,
+        displayName:
+          user.username ?? user.given_name ?? faker.internet.userName(),
+        email: user.email ?? "",
+        color: faker.helpers.arrayElement([
+          colors.emerald["500"],
+          colors.yellow["500"],
+          colors.green["500"],
+          colors.pink["500"],
+          colors.purple["500"],
+          colors.red["500"],
+          colors.amber["500"],
+        ]),
+      },
+    });
+  }
+
+  // redirect the user to dashboard page
+  return NextResponse.redirect(`${baseUrl}${routes.dashboard}`);
+}
